4f3008246eca27b8eaf3d3d20e3713ec4168934a
Claudio Meireles <166520790+ClaudioAMF1@users.noreply.github.com>
2024-09-29 15:17:34 -0300

    Create Pessoa.java

---
+++ trabalho_A1/Pessoa.java
@@ -0,0 +1,13 @@
+package trabalho_A1;
+
+public abstract class Pessoa {
+    protected String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
