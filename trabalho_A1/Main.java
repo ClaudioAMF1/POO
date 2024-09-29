0f1676a773b9b8a7e3b77047e7920ccc5402e27f
Claudio Meireles <166520790+ClaudioAMF1@users.noreply.github.com>
2024-09-29 15:17:12 -0300

    Create Main.java

---
+++ trabalho_A1/Main.java
@@ -0,0 +1,8 @@
+package trabalho_A1;
+
+public class Main {
+    public static void main(String[] args) {
+        Ensalamento ensalamento = new Ensalamento();
+        ensalamento.executarEnsalamento();
+    }
+}
