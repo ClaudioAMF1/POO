b1e8468473c5f0ca094468cd7189ec0624045fa4
Claudio Meireles <166520790+ClaudioAMF1@users.noreply.github.com>
2024-10-03 17:31:28 -0300

    Update Turma.java

--- trabalho_A1/Turma.java
+++ trabalho_A1/Turma.java
@@ -14,16 +14,14 @@ public class Turma {
         this.alunos = new ArrayList<>();
     }
 
-    // Adicionar alunos à turma
     public void adicionarAluno(Aluno aluno) {
         if (aluno.getCurso().equals(disciplina.getCurso())) {
             alunos.add(aluno);
         }
     }
 
-    // Exibir informações da turma
     public void mostrarInformacoes() {
-        System.out.println("Turma de " + disciplina.getNome());
+        System.out.println("Turma de " + disciplina.getNome() + " do curso " + disciplina.getCurso().getNome());
         System.out.println("Professor: " + professor.getNome());
         System.out.println("Alunos matriculados:");
         for (Aluno aluno : alunos) {
